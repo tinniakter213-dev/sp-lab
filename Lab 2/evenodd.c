4dc6e828dcc0a3b9e802be0f329b9ef77358966a
Fahmida kalam tinni <tinniakter213@gmail.com>
2026-02-18 10:32:11 +0600

    Create evenodd.c

---
+++ Lab 2/evenodd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("enter the value");
+    scanf("%d",&number);
+    if(number%2==0)
+    {
+        printf("the number is even");
+    }
+    else
+    {
+        printf("the number is od");
+    }
+    return 0;
+
+}
